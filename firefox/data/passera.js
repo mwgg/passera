6929ab3e025bbf53354f390ad974d1128e5e51ff
mwgg <m@mw.gg>
2014-09-27 13:36:21 +0400

    firefox addon code refactored

--- firefox/data/passera.js
+++ firefox/data/passera.js
@@ -1,49 +1,57 @@
-var pw1 = document.getElementById("pw1");
-var pw2 = document.getElementById("pw2");
-var pwlen = document.getElementById("pwlen");
-var charsbox = document.getElementById("charsbox");
-var showing = document.getElementById("showing");
-var show = document.getElementById("show");
-var hide = document.getElementById("hide");
+var mwggPasseraGen = {
+    init: function() {
+        mwggPasseraGen.pw1 = document.getElementById("pw1");
+        mwggPasseraGen.pw2 = document.getElementById("pw2");
+        mwggPasseraGen.pwlen = document.getElementById("pwlen");
+        mwggPasseraGen.charsbox = document.getElementById("charsbox");
+        mwggPasseraGen.showing = document.getElementById("showing");
+        mwggPasseraGen.show = document.getElementById("show");
+        mwggPasseraGen.hide = document.getElementById("hide");
+        
+        mwggPasseraGen.show.addEventListener('click', mwggPasseraGen.pwGen, false);
+        
+        mwggPasseraGen.hide.addEventListener('click', function(e) {
+            mwggPasseraGen.showing.innerHTML="&nbsp;";
+            mwggPasseraGen.hide.innerHTML="&nbsp;";
+        }, false);
 
-show.addEventListener('click', pwGen, false);
+    },
+    pwGen: function() {
+        this.phrase = mwggPasseraGen.pw1.value;
+        this.phraserep = mwggPasseraGen.pw2.value;
+        this.len = mwggPasseraGen.pwlen.value;
+        this.chars = mwggPasseraGen.charsbox.checked;
+        
+        if (this.phrase != this.phraserep && this.phraserep.length > 0) {
+            mwggPasseraGen.showing.innerHTML="Passwords did not match.";
+            return;
+        }
+        
+        if (this.len > 64) {
+            this.len = 64;
+            mwggPasseraGen.pwlen.value = "64";
+        }
+        
+        if (this.len < 4) {
+            this.len = 4
+            mwggPasseraGen.pwlen.value = "4";
+        }
+        
+        mwggPasseraGen.pw1.value="";
+        mwggPasseraGen.pw2.value="";
+        mwggPasseraGen.showing.innerHTML="Please wait...";
+        
+        this.worker = new Worker('hasher.js');
+        this.worker.postMessage( {"phrase":this.phrase, "len":this.len, "chars":this.chars} );
 
-hide.addEventListener('click', function(e) {
-    showing.innerHTML="&nbsp;";
-    hide.innerHTML="&nbsp;";
-}, false);
+        this.worker.onmessage = function (e) {
+            while (mwggPasseraGen.showing.firstChild) {
+                mwggPasseraGen.showing.removeChild(mwggPasseraGen.showing.firstChild);
+            }
+            mwggPasseraGen.showing.appendChild(document.createTextNode(e.data.pwd));
+            mwggPasseraGen.hide.innerHTML="[hide]";
+        };
+    }
+};
 
-function pwGen() {
-    var phrase = pw1.value;
-    var phraserep = pw2.value;
-    var len = pwlen.value;
-    var chars = charsbox.checked;
-    
-    if (phrase != phraserep && phraserep.length > 0) {
-        showing.innerHTML="Passwords did not match.";
-        return;
-    }
-    
-    if (len > 64) {
-        len = 64;
-        pwlen.value = "64";
-    }
-    
-    if (len < 4) {
-        len = 4
-        pwlen.value = "4";
-    }
-    
-    pw1.value="";
-    pw2.value="";
-    
-    showing.innerHTML="Please wait...";
-    
-    var worker = new Worker('hasher.js');
-    worker.postMessage( {"phrase":phrase, "len":len, "chars":chars} );
-    
-    worker.addEventListener('message', function(e) {
-        showing.innerHTML=e.data.pwd;
-        hide.innerHTML="[hide]";
-    }, false);
-}
+mwggPasseraGen.init();
--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,36 +1,39 @@
-var self = require("sdk/self");
-var { ActionButton } = require("sdk/ui/button/action");
-var { Panel } = require("sdk/panel");
-var { ToggleButton } = require('sdk/ui/button/toggle');
+var mwggPassera = {
+    init: function() {
+        mwggPassera.self = require("sdk/self");
+        
+        mwggPassera.passera = require("sdk/panel").Panel({
+          width: 200,
+          height: 400,
+          contentURL: mwggPassera.self.data.url("passera.html"),
+          contentScriptFile: mwggPassera.self.data.url("passera.js"),
+          contentStyleFile: mwggPassera.self.data.url("style.css"),
+          onHide: mwggPassera.handleHide
+        });
+        
+        mwggPassera.button = require('sdk/ui/button/toggle').ToggleButton({
+          id: "passera-button",
+          label: "Passera",
+          icon: {
+            "16": "./icon-16.png",
+            "32": "./icon-32.png",
+            "64": "./icon-64.png"
+          },
+          onClick: mwggPassera.openPassera
+        });
+    },
+    openPassera: function(state) {
+        if (state.checked) {
+          mwggPassera.passera.show({
+            position: mwggPassera.button
+          });
+        }
+    },
+    handleHide: function() {
+        mwggPassera.button.state('window', {checked: false});
+    }
 
-var passera = Panel({
-  width: 200,
-  height: 400,
-  contentURL: self.data.url("passera.html"),
-  contentScriptFile: self.data.url("passera.js"),
-  contentStyleFile: self.data.url("style.css"),
-  onHide: handleHide
-});
 
-var button = ToggleButton({
-  id: "passera-button",
-  label: "Passera",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
-  onClick: openPassera
-});
+};
 
-function openPassera(state) {
-    if (state.checked) {
-    passera.show({
-      position: button
-    });
-  }
-}
-
-function handleHide() {
-  button.state('window', {checked: false});
-}
+mwggPassera.init();
